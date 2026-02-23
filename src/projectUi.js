4098853bd5c65c68a0e56b07718b0685754cc328
0xsaidroot <nouredineahmad486@gmail.com>
2026-02-23 15:28:19 +0100

    modal not working

--- src/projectUi.js
+++ src/projectUi.js
@@ -1,4 +1,7 @@
- const projectList = document.createElement('ul');
+import { Project, saveProject, deleteProject } from "./project.js";
+
+const projectList = document.createElement('ul');
+const todoList = document.createElement('ul');
 
 export function displayProjects(aside,projects) {
     for (const project of projects) {
@@ -7,5 +10,27 @@ export function displayProjects(aside,projects) {
         projectItem.textContent = project.name;
         projectList.append(projectItem);
     }
+    aside.replaceChildren();
     aside.append(projectList);
+}
+
+export function displayTodos(aside,main,projects){
+
+    aside.addEventListener('click', function (event) {
+    let target = event.target.id;
+    let projectItem = projects.find(item => item.id === target);
+
+    if(!projectItem) return; 
+    console.log(projectItem);
+
+    todoList.replaceChildren();
+
+    for (const todo of projectItem.todoArray) {
+        const todoItem = document.createElement('li');
+        todoItem.textContent = todo.title;
+        todoList.append(todoItem);
+    }
+    
+    main.append(todoList);
+})
 }
--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 import "./styles.css";
 import { Todo, saveTodoToProject, deleteTodoFromProject } from './todos.js';
 import { Project, saveProject, deleteProject } from "./project.js";
-import { displayProjects } from "./projectUi.js";
-import { displayTodos } from "./todoUi.js";
+import { displayProjects, displayTodos } from "./projectUi.js";
+
 
 const projects = [];
 let project1 = new Project();
@@ -20,11 +20,35 @@ saveProject(project2, projects);
 console.log({ projects })
 
 
-
-
 const aside = document.querySelector('aside');
 const main = document.querySelector('main');
+const dialog = document.querySelector('dialog');
 
-displayProjects(aside,projects);
-displayTodos(aside,main,projects);
+displayProjects(aside, projects);
+displayTodos(aside, main, projects);
 
+const showDialog = (show) =>
+    show ? dialog.showModal() : dialog.close();
+
+aside.addEventListener('click', (event) => {
+    let target = event.target;
+
+    if (target.id === 'openDialog') {
+        showDialog(true);
+    } else return;
+
+})
+
+dialog.addEventListener('click', function (event) {
+    let target = event.target.id;
+
+    if (target === "cancelBtn") showDialog(close);
+    else if (target === "addBtn") {
+        const newProjectName = document.querySelector('projectName');
+        const newProject = new Project(newProjectName.value);
+        saveProject(newProject, projects);
+        displayProjects(aside, projects);
+    }
+    else return;
+
+})
